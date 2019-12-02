2f039b6a3c190c45d1ec3b9e4c7bd19e74afa942
kate_gaw <ekaterinagaw@gmail.com>
2019-12-02 16:03:20 +0300

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,11 @@
 export default function StringToNumberFunc(input) {
-    let output = '';
-    try {
-        if (Number.isNaN(input) === false && Number.parseInt(input, 10) === input) {
-            output = input;
-        } else throw input;
-    } catch (e) {
-        output = `Error: ${input} is not an integer in decimal`;
-    }
-    return output;
+  let output = '';
+  try {
+    if (Number.isNaN(input) === false && Number.parseInt(input, 10) === input) {
+      output = input;
+    } else throw input;
+  } catch (e) {
+    output = `Error: ${input} is not an integer in decimal`;
+  }
+  return output;
 }
--- src/js/test.js
+++ src/js/test.js
@@ -1,25 +1,25 @@
 import StringToNumberFunc from './app';
 
 test('if input is not a decimal', () => {
-    const input = 3;
+  const input = 3;
 
-    const expected = 3;
-    const recieved = StringToNumberFunc(input);
-    expect(recieved).toBe(expected);
+  const expected = 3;
+  const recieved = StringToNumberFunc(input);
+  expect(recieved).toBe(expected);
 });
 
 test('if input is not a number', () => {
-    const input = 'hello';
+  const input = 'hello';
 
-    const expected = 'Error: hello is not an integer in decimal';
-    const recieved = StringToNumberFunc(input);
-    expect(recieved).toBe(expected);
+  const expected = 'Error: hello is not an integer in decimal';
+  const recieved = StringToNumberFunc(input);
+  expect(recieved).toBe(expected);
 });
 
 test('if input is not decimal', () => {
-    const input = '2AE';
+  const input = '2AE';
 
-    const expected = 'Error: 2AE is not an integer in decimal';
-    const recieved = StringToNumberFunc(input);
-    expect(recieved).toBe(expected);
+  const expected = 'Error: 2AE is not an integer in decimal';
+  const recieved = StringToNumberFunc(input);
+  expect(recieved).toBe(expected);
 });
